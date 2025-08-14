39fc78c9408633d2ec6430626eb317cb033ff0a7
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-08-14 17:02:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
-const inputs = document.querySelectorAll('.controls input');
+let video=document.getElementById("video");
+let playButton=document.querySelector(".player_button.toggle");
+let volumeButton=document.getElementById("volume");
+let playbackspeedButton=document.getElementById("playbackspeed");
+let progress=document.getElementById("progress")
+let backwardButton=document.getElementById("backward");
+let forwardButton=document.getElementById("forward")
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+ 
+playButton.addEventListener("click",(e)=>{
+  if(video.paused || video.ended){
+    video.play();
+    playButton.textContent="❚ ❚"
+  
+  }
+  else{
+    video.pause();
+    playButton.textContent="►"
+  }
+})
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+volumeButton.addEventListener("input",()=>{
+  console.log(volumeButton.value);
+  video.volume=volumeButton.value;
+})
+
+// video.addEventListener("loadedmetadata",()=>{
+//   progress.setAttribute("max",video.duration)
+
+  
+// })
+
+// progress.setAttribute("max", video.duration);
+video.addEventListener("loadedmetadata", () => {
+  progress.setAttribute("max", video.duration);
+});
+
+
+video.addEventListener("timeupdate",()=>{
+  progress.value=video.currentTime;
+})
+playbackspeedButton.addEventListener("input",()=>{
+  video.playbackRate=playbackspeedButton.value;
+})
+backwardButton.addEventListener("click",()=>{
+  let backwardTime=backwardButton.value;
+  video.currentTime=video.currentTime*(-backwardTime);
+  progress.value=video.currentTime*backwardTime;
+})
+
+forwardButton.addEventListener("click",()=>{
+  let forwardTime=forwardButton.value;
+  video.currentTime=video.currentTime*(forwardTime);
+  progress.value=video.currentTime*forwardTime;
+})
+
+
+
+
+
